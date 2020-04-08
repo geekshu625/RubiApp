100e098cfc1ff1b9fbec9fe67a048193954049ac
geekshu625 <efi11983@kwansei.ac.jp>
2020-04-08 23:27:08 +0900

    プロジェクト作成&gitignoreを追加

---
+++ RubiApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RubiApp
+//
+//  Created by 松木周 on 2020/04/08.
+//  Copyright © 2020 ShuMatsuki. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
