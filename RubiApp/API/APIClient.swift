e3d3ebcd1f8ae1fde841786fb5e48727660ff83e
geekshu625 <efi11983@kwansei.ac.jp>
2020-04-09 18:58:23 +0900

    ObservableなレスポンスをするようにAPIClientを修正

--- RubiApp/API/APIClient.swift
+++ RubiApp/API/APIClient.swift
@@ -16,8 +16,28 @@ class HiraganaAPIClient {
         
         return Observable.create { [weak self] observer -> Disposable in
             guard let `self` = self else { return Disposables.create {} }
-
+            //TODO: リファクタリング
+            var urlRequest = URLRequest(url: URL(string: request.path)!)
+            urlRequest.httpMethod = request.method.rawValue
+            urlRequest.addValue("application/json", forHTTPHeaderField: "Content-Type")
+            let body = try? JSONEncoder().encode(request.body)
+            
+            let task = AF.upload(body!, with: urlRequest)
+                .response { (response) in
+                    guard let data = response.data else { return }
+                    do {
+                        //TODO: API側のエラー処理
+                        let hiragana = try JSONDecoder().decode(Hiragana.self, from: data)
+                        DispatchQueue.main.async {
+                            observer.onNext(hiragana as! Request.ResponseObject)
+                        }
+                    } catch {
+                        //TODO: ネットワークのエラー処理を追加
+                    }
+            }
+            task.resume()
             return Disposables.create {
+                task.cancel()
             }
         }
     }
