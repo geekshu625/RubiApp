0db72e525bac9fd299e2e6d6538a12443629b4c0
ShuMatsuki <shu.matsuki.0625@gmail.com>
2020-05-27 16:34:29 +0900

    HomeUsecaseを追加

---
+++ RubiApp/Domain/Usecase/HomeUsecase.swift
@@ -0,0 +1,68 @@
+//
+//  HomeUsecase.swift
+//  RubiApp
+//
+//  Created by 松木周 on 2020/05/27.
+//  Copyright © 2020 ShuMatsuki. All rights reserved.
+//
+
+import Foundation
+import RxSwift
+import Alamofire
+
+//TODO: Usecase層に責務としてここに持たせるべきではない、ViewControllerかViewModelで宣言するべき
+protocol HomeConvertUsecaseProtocl: AnyObject {
+    func postKanzi<Request: APIRequest>(request: Request) -> Observable<Request.ResponseObject>
+}
+
+final class HomeUsecase {}
+
+extension HomeUsecase: HomeConvertUsecaseProtocl {
+
+    func postKanzi<Request: APIRequest>(request: Request) -> Observable<Request.ResponseObject> {
+
+        return Observable.create { [weak self] observer -> Disposable in
+            guard let `self` = self else { return Disposables.create {} }
+
+            var urlRequest = URLRequest(url: URL(string: request.path)!)
+            urlRequest.httpMethod = request.method.rawValue
+            urlRequest.addValue("application/json", forHTTPHeaderField: "Content-Type")
+            let body = try? JSONEncoder().encode(request.body)
+
+            let task = AF.upload(body!, with: urlRequest)
+                .response { (response) in
+                    switch response.result {
+                    case .success:
+                        guard let data = response.data else { return }
+
+                        //APIのエラー処理
+                        if let errorModel = try? JSONDecoder().decode(APIErrorRooter.self, from: data) {
+                            let apiError = HiraganaAPIError.init(errorCode: errorModel.error.code)
+                            observer.onError(apiError)
+                        }
+
+                        let hiragana = try? JSONDecoder().decode(ConvertedResponse.self, from: data)
+                        DispatchQueue.main.async {
+                            //swiftlint:disable:next force_cast
+                            observer.onNext(hiragana as! Request.ResponseObject)
+                        }
+
+                    case .failure(let error):
+                        //ネットワークのエラー処理
+                        let connectingError = ConnectionError(errorCode: error._code)
+                        observer.on(.error(connectingError))
+                    }
+            }
+
+            task.resume()
+
+            return Disposables.create {
+                task.cancel()
+            }
+
+        }
+
+    }
+
+}
+
