c8345ea0a9ced588bffcc884860930c8eb9190a1
ShuMatsuki <shu.matsuki.0625@gmail.com>
2020-05-28 12:44:14 +0900

    ビルドスクリプトにDIKitGenを追加し生成されたコードの出力先ファイルも作成

---
+++ RubiApp/AutoGenerated/DIKit/AppResolver.generated.swift
@@ -0,0 +1,11 @@
+//
+//  AppResolver.generated.swift
+//  RubiApp
+//
+//  Created by 松木周 on 2020/05/28.
+//  Copyright © 2020 ShuMatsuki. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import DIKit
