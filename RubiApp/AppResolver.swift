bbe460ce59d7ebfd477ef95777c3b35ce814a22d
ShuMatsuki <shu.matsuki.0625@gmail.com>
2020-05-28 17:57:17 +0900

    HomeViewControllerで使用するHomeViewModelをDI化

---
+++ RubiApp/AppResolver.swift
@@ -0,0 +1,28 @@
+//
+//  AppResolver.swift
+//  RubiApp
+//
+//  Created by 松木周 on 2020/05/28.
+//  Copyright © 2020 ShuMatsuki. All rights reserved.
+//
+
+import Foundation
+import DIKit
+
+protocol AppResolver: DIKit.Resolver {
+    func provideResolver() -> AppResolver
+
+    func provideHomeViewModel() -> HomeViewModel
+}
+
+final class AppResolverImpl: AppResolver {
+
+    func provideResolver() -> AppResolver {
+        return self
+    }
+
+    func provideHomeViewModel() -> HomeViewModel {
+        return HomeViewModel(homeConvertUsecase: HomeUsecase())
+    }
+
+}
