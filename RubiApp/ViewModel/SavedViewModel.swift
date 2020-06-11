7a7c224b68c61101d1d28b220dfd24c17e8cfc6e
ShuMatsuki <shu.matsuki.0625@gmail.com>
2020-06-11 13:16:53 +0900

    SavedViewModelからRxDatasoucesを取り除く

--- RubiApp/ViewModel/SavedViewModel.swift
+++ RubiApp/ViewModel/SavedViewModel.swift
@@ -9,48 +9,20 @@
 import Foundation
 import RxSwift
 import RxCocoa
-import RxDataSources
 
-struct SavedTableViewData {
-    let id = UUID().uuidString
-    let vocabulary: Savelist
-}
+class SavedViewModel {
 
-extension SavedTableViewData: IdentifiableType, Equatable {
-    var identity: String {return id}
-    static func == (lhs: SavedTableViewData, rhs: SavedTableViewData) -> Bool {
-        return lhs.identity == rhs.identity
-    }
-}
+    var convertedInfo = [ConvertedInfo]()
 
-class SavedViewModel: ListViewModelProtocol {
-
-    typealias Data = SavedTableViewData
-    typealias SectionModel = AnimatableSectionModel<Int, Data>
-
-    var dataObservable: Observable<[SectionModel]> {
+    var dataObservable: Observable<[ConvertedInfo]> {
         return dataRelay.asObservable()
     }
-    private let dataRelay = BehaviorRelay<[SectionModel]>(value: [])
+    private let dataRelay = BehaviorRelay<[ConvertedInfo]>(value: [])
     private let disposeBag = DisposeBag()
-
-    private func toSectionModel(shouldRefresh: Bool = false, type: Data) {
-        var preItems = dataRelay.value.first?.items ?? []
-        preItems.append(type)
-        let items = shouldRefresh ? [type] : preItems
-        let section = 0
-        let sectionModel  = SectionModel(model: section, items: items)
-        dataRelay.accept([sectionModel])
-    }
 
     //RealmDBから全データを取得する
     func fetchAllVocabulary() {
-        remove()
-        let vocabularies = SavelistManager.getAll()
-        for vocabulary in vocabularies {
-            let data = Data(vocabulary: vocabulary)
-            self.toSectionModel(type: data)
-        }
+
     }
 
     //RealmDBから個別データ削除
@@ -61,17 +33,6 @@ class SavedViewModel: ListViewModelProtocol {
     //RealmDBから全データ削除
     func deleteAllVocabulary() {
         SavelistManager.deleteAll()
-        remove()
-    }
-
-    //データが2重追加されるのを防止するために、一度空のデータをストリームに流して白紙になるように上書きする
-    private func remove() {
-        var preItems = dataRelay.value.first?.items ?? []
-        guard preItems.count > 0 else {return}
-        preItems.removeAll()
-        let section = 0
-        let sectionModel = SectionModel(model: section, items: preItems)
-        dataRelay.accept([sectionModel])
     }
 
 }
